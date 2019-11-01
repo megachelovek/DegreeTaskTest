adadc0f5289fbe27bb571489761e16dde83812ee
Danila Malchikov <Danila Malchikov>
2019-11-01 20:04:35 +0400

    [*] #master Первый коммит

---
+++ ExamNorm/CalculateTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamNorm
+{
+    class CalculateTime
+    {
+        readonly int hour;
+        readonly int minute;
+
+        public CalculateTime(int hours, int minutes)
+        {
+            this.minute = minutes;
+            this.hour = hours;
+        }
+
+        public double CalculateDegreesMain()
+        {
+            double hourDegrees = this.hour * 30 + this.minute * 0.5;
+            double minuteDegrees = this.minute * 6;
+            double result = Math.Abs(hourDegrees - minuteDegrees); 
+            return (result == 360) ? 0: result;
+        }
+    }
+}
---
+++ ExamNorm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExamNorm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flagMinute = false, flagHour = false;
+            int hour =0, minute=0;
+            while (!flagHour) {
+                Console.WriteLine("Введите часы от 1 до 12 (Например: 4)");
+                var hourString = Console.ReadLine();
+                if (int.TryParse(hourString, out int temp))
+                {
+                    if (temp >= 1 && temp <= 12)
+                    {
+                        flagHour = true;
+                        hour = temp;
+                    }
+                }
+            }
+
+            while (!flagMinute)
+            {
+                Console.WriteLine("Введите минуты от 0 до 59 (Например: 42)");
+                var minuteString = Console.ReadLine();
+                if (int.TryParse(minuteString, out int temp))
+                {
+                    if (temp >= 0
+                        && temp <= 59)
+                    {
+                        flagMinute = true;
+                        minute = temp;
+                    }
+                }
+            }
+
+            CalculateTime calculateTime = new CalculateTime(hour, minute);
+            Console.WriteLine($"Между ними разница в {calculateTime.CalculateDegreesMain()} градусов");
+            Console.ReadKey();
+        }
+    }
+}
